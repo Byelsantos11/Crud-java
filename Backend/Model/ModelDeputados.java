049556aa4745fe02a1766c7679f9a88fded7ca57
Gabryel Rodrigues Dos Santos <144414721+Byelsantos11@users.noreply.github.com>
2024-06-11 23:36:22 -0300

    Create ModelDeputados.java

---
+++ Backend/Model/ModelDeputados.java
@@ -0,0 +1,29 @@
+public class ModelDeputados {
+ private int id;
+ private String nome;
+ private String email;
+
+
+
+public int getId(){
+    return id;
+}
+
+public void SetId(int id){
+    this.id=id;
+}
+
+public String getNome(){
+    return nome;
+}
+
+
+public void SetNome(String nome){
+    this.nome=nome;
+}
+
+public String getEmail(){
+    return email;
+}
+
+}
